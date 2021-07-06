233837f859b2761f5e04f47dee0f2f9400bf2e2d
YangJJ <891896039@qq.com>
2021-07-06 16:36:25 +0800

    添加复选框文件

--- src/components/june-checkbox/j-checkbox.vue
+++ src/components/june-checkbox/j-checkbox.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    about
-  </div>
+    <div>
+
+    </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+    export default {
+        
+    }
 </script>
 
 <style lang="scss" scoped>
