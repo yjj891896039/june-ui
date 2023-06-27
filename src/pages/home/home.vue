e4df3e9017eb9a4762ca2ab090b5800e0a077ebc
GoodMood <891896039@qq.com>
2023-06-28 00:52:40 +0800

    home page version 1

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    home
+    home is home page (version1)
   </div>
 </template>
 
@@ -8,7 +8,7 @@
   export default {
     data() {
       return {
-        timerIds: []
+        timerIds: [1,2,3,4]
       }
     },
     mounted () {
